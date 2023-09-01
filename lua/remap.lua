cb34d4a65a723bedfe3fadac1bc77e201cb8a667
Morgan Bentell <mbentell@spotify.com>
2023-09-01 10:09:04 +0200

    some stuff

--- lua/remap.lua
+++ lua/remap.lua
@@ -48,3 +48,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>b", "<C-o>")
--- todo.md
+++ todo.md
@@ -7,3 +7,4 @@
 [x] terminal (not with vim)
 [x] find usages
 [ ] refactor: https://github.com/ThePrimeagen/refactoring.nvim
+[ ] prettier
