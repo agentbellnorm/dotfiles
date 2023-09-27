5de81f88cbfae1c85c245d6edcc6b917b21f801a
Morgan Bentell <mbentell@spotify.com>
2023-09-27 10:55:06 +0200

    split window vertically

--- lua/remap.lua
+++ lua/remap.lua
@@ -62,3 +62,6 @@ vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
+
+-- split window vertically
+vim.keymap.set("n", "<leader>wv", "<C-w>v")
