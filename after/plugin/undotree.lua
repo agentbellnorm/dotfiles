9145980e00e388b2f698c5ccc54c2e04a7ed9f14
Morgan Bentell <morgan.bentell@gmail.com>
2023-10-27 09:26:26 +0200

    hoping to make undotree work better

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,2 +1,17 @@
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
+vim.cmd([[
+    if has("persistent_undo")
+        let target_path = expand('~/.undodir')
+        " create the directory and any parent directories
+        " if the location does not exist.
+        if !isdirectory(target_path)
+           call mkdir(target_path, "p", 0700)
+        endif
+        
+        let &undodir=target_path
+        set undofile
+    endif
+]])
+
+vim.g.undotree_WindowLayout = 3
