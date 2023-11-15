171b77293fb9609afe04e9cd57189d814c10e155
Morgan Bentell <mbentell@spotify.com>
2023-11-15 12:01:28 +0100

    Remove print when attaching lsp, it messed up things in the cmdline

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -29,7 +29,6 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-    print('lsp attach')
     local opts = { buffer = bufnr, remap = false }
 
     vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
