d72a6011fd49366b51c3a5db0cff27902f4f1264
Morgan Bentell <mbentell@spotify.com>
2023-11-30 13:22:20 -0500

    fix copilot

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -22,6 +22,19 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<C-Space'] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        local copilot_keys = vim.fn['copilot#Accept']()
+        if cmp.visible() then
+            cmp.select_next_item()
+        elseif copilot_keys ~= '' and type(copilot_keys) == 'string' then
+            vim.api.nvim_feedkeys(copilot_keys, 'i', true)
+        else
+            fallback()
+        end
+    end, {
+        'i',
+        's',
+    }),
 })
 
 lsp.setup_nvim_cmp({
