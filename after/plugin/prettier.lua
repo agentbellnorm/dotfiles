0977aeb85001634ae1ca6ba0b4d5dd694b35193e
Morgan Bentell <morgan.bentell@gmail.com>
2024-02-19 22:13:43 +0100

    cleanup

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -1,13 +1,9 @@
 local js_utils = require("js-helpers")
-
--- https://github.com/MunifTanjim/prettier.nvim#setting-up-prettiernvim
 local null_ls = require("null-ls")
-
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
-
 local prettier = require("prettier")
+
+-- Based on the plugin readme:
+-- https://github.com/MunifTanjim/prettier.nvim#setting-up-prettiernvim
 
 if js_utils.is_prettier() then
   print("Using prettier!")
