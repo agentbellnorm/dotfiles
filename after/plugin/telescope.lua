a217316c3da353e712ac00d264daa3882bb9935b
Morgan Bentell <morgan.bentell@gmail.com>
2024-03-18 07:34:05 +0100

    live grep of visually selected text

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,11 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+vim.keymap.set('v', '<leader>pg', function()
+  vim.cmd.normal('"fy')
+  builtin.grep_string({ search = vim.fn.getreg('"f') })
 end)
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader><TAB>', builtin.buffers, {})
 
 
