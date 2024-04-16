ce13818da453820acc352a978eda400ad54914a6
Morgan Bentell <morgan.bentell@insurely.com>
2024-04-16 18:09:43 +0200

    less transparency in neovide

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -16,8 +16,8 @@ if vim.g.neovide then
     return string.format("%x", math.floor((255 * vim.g.transparency) or 0.9))
   end
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.9
-  vim.g.transparency = 0.9
+  vim.g.neovide_transparency = 0.88
+  vim.g.transparency = 0.88
   vim.g.neovide_window_blurred = true
   -- vim.g.neovide_background_color = "#0f1117" .. alpha()
 
