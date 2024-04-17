07511ca3f997611fe38ba05c51d31f494028fc92
Morgan Bentell <morgan.bentell@insurely.com>
2024-04-17 16:52:08 +0200

    get signature hints, disable java for now

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,78 +1,84 @@
 local lsp = require("lsp-zero")
 local lspconfig = require("lspconfig")
 local js_helpers = require("js-helpers")
-local java = require('java')
+-- local java = require('java')
 
 lsp.preset('recommended')
 
 -- available servers: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
-  'rust_analyzer',
-  'lua_ls',
-  'bashls',
-  'pyright',
-  'biome',
-  'jdtls',
-  'html',
-  'wgsl_analyzer',
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+    'lua_ls',
+    'bashls',
+    'pyright',
+    'biome',
+    -- 'jdtls',
+    'html',
+    'wgsl_analyzer',
 })
 
 local cmp = require("cmp")
+local cmp_format = lsp.cmp_format({ details = true })
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  ['<C-Space'] = cmp.mapping.complete(),
-  ['<Tab>'] = cmp.mapping(function(fallback)
-    fallback()
-  end, { 'i', 's' }),
-})
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space'] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        fallback()
+    end, { 'i', 's' }),
 })
 
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings,
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' }
+    },
+    formatting = cmp_format
+})
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set('n', 'gd', function() require('telescope.builtin').lsp_definitions() end, opts)
-  vim.keymap.set('n', 'gi', function() require('telescope.builtin').lsp_implementations() end, opts)
-  vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-  -- open error, full error message
-  vim.keymap.set('n', '<leader>e', function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-  -- code action
-  vim.keymap.set('n', '<leader>a', function() vim.lsp.buf.code_action() end, opts)
-  -- show references
-  vim.keymap.set('n', 'F', function() require('telescope.builtin').lsp_references() end,
-    { noremap = true, silent = true, buffer = bufnr })
-  -- search in buffer
-  vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end,
-    { noremap = true, silent = true, buffer = bufnr })
-  -- rename variable
-  vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', 'gd', function() require('telescope.builtin').lsp_definitions() end, opts)
+    vim.keymap.set('n', 'gi', function() require('telescope.builtin').lsp_implementations() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- open error, full error message
+    vim.keymap.set('n', '<leader>e', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    -- code action
+    vim.keymap.set('n', '<leader>a', function() vim.lsp.buf.code_action() end, opts)
+    -- show references
+    vim.keymap.set('n', 'F', function() require('telescope.builtin').lsp_references() end,
+        { noremap = true, silent = true, buffer = bufnr })
+    -- search in buffer
+    vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end,
+        { noremap = true, silent = true, buffer = bufnr })
+    -- rename variable
+    vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 
-  if client.supports_method("textDocument/formatting") and not js_helpers.is_eslint_or_prettier() then
-    print("format!!")
-    vim.keymap.set("n", "<leader>f", function()
-      vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-    end, opts)
-  end
+    if client.supports_method("textDocument/formatting") and not js_helpers.is_eslint_or_prettier() then
+        print("format!!")
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        end, opts)
+    end
 
-  if client.supports_method("textDocument/rangeFormatting") and not js_helpers.is_eslint_or_prettier() then
-    vim.keymap.set("x", "<leader>f", function()
-      vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-    end, opts)
-  end
+    if client.supports_method("textDocument/rangeFormatting") and not js_helpers.is_eslint_or_prettier() then
+        vim.keymap.set("x", "<leader>f", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        end, opts)
+    end
 end)
 
-java.setup()
-lspconfig.jdtls.setup({})
+-- java.setup()
+-- lspconfig.jdtls.setup({})
 
 lsp.setup()
 
@@ -82,13 +88,14 @@ lsp.setup()
 
 -- disable autoformatting with tsserver, (using prettier/eslint instead)
 lspconfig.tsserver.setup({
-  on_init = function(client)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentFormattingRangeProvider = false
-  end
+    on_init = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentFormattingRangeProvider = false
+    end
 })
 
 vim.cmd([[
     au BufNewFile,BufRead *.wgsl set filetype=wgsl
     ]]
 )
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,100 +1,101 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.2',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    'nvim-java/nvim-java',
-    dependencies = {
-      'nvim-java/lua-async-await',
-      'nvim-java/nvim-java-core',
-      'nvim-java/nvim-java-test',
-      'nvim-java/nvim-java-dap',
-      'MunifTanjim/nui.nvim',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-dap',
-      {
-        'williamboman/mason.nvim',
-        opts = {
-          registries = {
-            'github:nvim-java/mason-registry',
-            'github:mason-org/mason-registry',
-          },
-        },
-      }
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
-  },
-  { 'nvim-telescope/telescope-ui-select.nvim' },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      vim.cmd('colorscheme rose-pine')
-    end
-  },
-  { 'nvim-treesitter/nvim-treesitter',        build = ':TSUpdate' },
-  { 'mbbill/undotree' },
-  { 'tpope/vim-fugitive' },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    -- {
+    --     'nvim-java/nvim-java',
+    --     dependencies = {
+    --         'nvim-java/lua-async-await',
+    --         'nvim-java/nvim-java-core',
+    --         'nvim-java/nvim-java-test',
+    --         'nvim-java/nvim-java-dap',
+    --         'MunifTanjim/nui.nvim',
+    --         'neovim/nvim-lspconfig',
+    --         'mfussenegger/nvim-dap',
+    --         {
+    --             'williamboman/mason.nvim',
+    --             opts = {
+    --                 registries = {
+    --                     'github:nvim-java/mason-registry',
+    --                     'github:mason-org/mason-registry',
+    --                 },
+    --             },
+    --         }
+    --     },
+    -- },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
+    { 'nvim-treesitter/nvim-treesitter',        build = ':TSUpdate' },
+    { 'mbbill/undotree' },
+    { 'tpope/vim-fugitive' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },       -- Required
+            { 'williamboman/mason.nvim' },     -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },     -- Required
-      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-      { 'L3MON4D3/LuaSnip' },     -- Required
-    }
-  },
-  {
-    "Pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup {
-        -- your config goes here
-        -- or just leave it empty :)
-        debounce_delay = 2000
-      }
-    end
-  },
-  { "neovim/nvim-lspconfig" },
-  { 'simrat39/rust-tools.nvim' },
-  {
-    'numToStr/Comment.nvim',
-    opts = {
-      -- add any options here
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
     },
-    lazy = false,
-  },
-  { 'lewis6991/gitsigns.nvim' },
-  { 'mg979/vim-visual-multi' },
-  { 'jose-elias-alvarez/null-ls.nvim' },
-  { 'MunifTanjim/prettier.nvim' },
-  { 'nvim-treesitter/nvim-treesitter-context' },
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      { 'nvim-tree/nvim-web-devicons' }
-    }
-  },
-  { 'axelvc/template-string.nvim' },
-  { 'github/copilot.vim' },
-  { 'nvim-lualine/lualine.nvim' },
-  {
-    "ThePrimeagen/refactoring.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+    { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+    {
+        "Pocco81/auto-save.nvim",
+        config = function()
+            require("auto-save").setup {
+                -- your config goes here
+                -- or just leave it empty :)
+                debounce_delay = 2000
+            }
+        end
     },
-    config = function()
-      require("refactoring").setup()
-    end,
-  },
-  { 'LunarWatcher/auto-pairs' },
-  { 'HiPhish/rainbow-delimiters.nvim' },
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+    { "neovim/nvim-lspconfig" },
+    { 'simrat39/rust-tools.nvim' },
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+        lazy = false,
+    },
+    { 'lewis6991/gitsigns.nvim' },
+    { 'mg979/vim-visual-multi' },
+    { 'jose-elias-alvarez/null-ls.nvim' },
+    { 'MunifTanjim/prettier.nvim' },
+    { 'nvim-treesitter/nvim-treesitter-context' },
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            { 'nvim-tree/nvim-web-devicons' }
+        }
+    },
+    { 'axelvc/template-string.nvim' },
+    { 'github/copilot.vim' },
+    { 'nvim-lualine/lualine.nvim' },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("refactoring").setup()
+        end,
+    },
+    { 'LunarWatcher/auto-pairs' },
+    { 'HiPhish/rainbow-delimiters.nvim' },
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
