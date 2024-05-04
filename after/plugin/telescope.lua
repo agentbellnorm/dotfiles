f9fbecc982a54fed92bd73937f2fae781b8ae507
Morgan Bentell <morgan.bentell@gmail.com>
2024-05-04 09:55:11 +0200

    change layout of telescope pickers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,8 +2,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('v', '<leader>pg', function()
-  vim.cmd.normal('"fy')
-  builtin.grep_string({ search = vim.fn.getreg('"f') })
+    vim.cmd.normal('"fy')
+    builtin.grep_string({ search = vim.fn.getreg('"f') })
 end)
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader><TAB>', builtin.buffers, {})
@@ -12,39 +12,58 @@ vim.keymap.set('n', '<leader><TAB>', builtin.buffers, {})
 -- For telescope-ui-select: https://github.com/nvim-telescope/telescope-ui-select.nvim
 
 require("telescope").setup {
-  pickers = {
-    buffers = {
-      sort_mru = true,
-      ignore_current_buffer = true,
-      layout_strategy = "vertical",
-      layout_config = {
-        mirror = true
-      }
+    pickers = {
+        buffers = {
+            sort_mru = true,
+            ignore_current_buffer = true,
+            layout_strategy = "vertical",
+            layout_config = {
+                mirror = true
+            }
+        },
+        find_files = {
+            layout_strategy = "vertical",
+            layout_config = {
+                mirror = true
+            }
+        },
+        live_grep = {
+            layout_strategy = "vertical",
+
+            layout_config = {
+                mirror = true
+            }
+        },
+        grep_string = {
+            layout_strategy = "vertical",
+            layout_config = {
+                mirror = true
+            }
+        },
+        lsp_references = {
+            layout_strategy = "vertical",
+            layout_config = {
+                mirror = true
+            }
+        },
+        lsp_definitions = {
+            layout_strategy = "vertical",
+            layout_config = {
+                mirror = true
+            }
+        },
+        lsp_implementations = {
+            layout_strategy = "vertical",
+            layout_config = {
+                mirror = true
+            }
+        }
     },
-    lsp_references = {
-      layout_strategy = "vertical",
-      layout_config = {
-        mirror = true
-      }
-    },
-    lsp_definitions = {
-      layout_strategy = "vertical",
-      layout_config = {
-        mirror = true
-      }
-    },
-    lsp_implementations = {
-      layout_strategy = "vertical",
-      layout_config = {
-        mirror = true
-      }
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+        }
     }
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {}
-    }
-  }
 }
 -- To get ui-select loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
