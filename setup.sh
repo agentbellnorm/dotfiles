74a0e361408cb84dc7db847a3c74e60cad5d608e
Morgan Bentell <morgan.bentell@insurely.com>
2024-12-06 13:07:58 +0100

    setup eslint_d

--- setup.sh
+++ setup.sh
@@ -6,23 +6,32 @@ ln -s "$(pwd)" ~/.config/nvim
 
 
 if ! [ -x "$(command -v rg)" ]; then
-  "ripgrep not installed, installing"
+  echo "ripgrep not installed, installing"
   brew install ripgrep
 else 
   echo '✅ ripgrep installed'
 fi
 
 if ! [ -x "$(command -v jq)" ]; then
-  "jq not installed, installing"
+  echo "jq not installed, installing"
   brew install jq
 else 
   echo '✅ jq installed'
 fi
 
 if ! [ -x "$(command -v prettierd)" ]; then
-  "prettierd not installed, installing"
+  echo "prettierd not installed, installing"
   npm install -g @fsouza/prettierd
 
 else 
   echo '✅ prettierd installed'
 fi
+
+if ! [ -x "$(command -v eslint_d)" ]; then
+  echo "eslint_d not installed, installing"
+  npm i -g eslint_d
+
+else 
+  echo '✅ eslint_d installed'
+fi
+
