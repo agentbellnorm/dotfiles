d2324479adff6033ff26e777fff8052d253f6995
Morgan Bentell <morgan.bentell@insurely.com>
2024-12-06 13:12:34 +0100

    fix conform js/ts config

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,3 +1,5 @@
+local js_ts_formatters = { "eslint_d", "eslint", "prettierd", "prettier", stop_after_first = true }
+
 require("conform").setup({
     formatters_by_ft = {
         lua = { "stylua" },
@@ -6,12 +8,17 @@ require("conform").setup({
         -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt", lsp_format = "fallback" },
         -- Conform will run the first available formatter
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        javascript = js_ts_formatters,
+        typescript = js_ts_formatters,
+        javascriptreact = js_ts_formatters,
+        typescriptreact = js_ts_formatters,
         json = { "prettierd", "prettier", stop_after_first = true },
         graphql = { "prettierd", "prettier", stop_after_first = true },
         java = { "google-java-format" },
     },
+    formatters = {
+        eslint_d = {
+            command = "eslint_d"
+        }
+    }
 })
